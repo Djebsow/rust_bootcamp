bd0740f38a463a41a14919249497c65b9bcc3073
Kevin DA SILVA RESENDE <kevin.dasilva2804@gmail.com>
2026-01-15 10:32:10 +0100

    changement sur le cd Rusty_Hello

--- Rusty_Hello/src/main.rs
+++ Rusty_Hello/src/main.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(name = "rusty_hello", about = "CLI arguments et ownership")]
 struct Args {
-    /// Nom pour saluer 
+    /// Nom pour saluer
     #[arg(default_value = "World")]
     name: String,
 
@@ -11,7 +11,7 @@ struct Args {
     #[arg(short, long)]
     upper: bool,
 
-    /// Répeter un nombre de fois 
+    /// Répeter un nombre de fois
     #[arg(short, long, default_value_t = 1)]
     repeat: u32,
 }
@@ -19,14 +19,14 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    // Gestion de la mise en majuscules (Ownership) 
+    // Gestion de la mise en majuscules (Ownership)
     let mut display_name = args.name; // c'est une string pour déplacer la donnée vers la variable display_name. On la rend mut pour possible modification
-    
+
     if args.upper {
         display_name = display_name.to_uppercase();
     }
 
-    // Boucle pour la répétition 
+    // Boucle pour la répétition
     for _ in 0..args.repeat {
         println!("Hello, {}!", display_name);
     }
