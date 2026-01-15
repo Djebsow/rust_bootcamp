d98a0c9f241ae9d464b8a5568cdbf9ce3ab172a6
Kevin DA SILVA RESENDE <kevin.dasilva2804@gmail.com>
2026-01-15 10:17:55 +0100

    Configuration Rusty_Hello

--- Rusty_Hello/src/main.rs
+++ Rusty_Hello/src/main.rs
@@ -1,3 +1,33 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "rusty_hello", about = "CLI arguments et ownership")]
+struct Args {
+    /// Nom pour saluer 
+    #[arg(default_value = "World")]
+    name: String,
+
+    /// Pour convertir en majuscule
+    #[arg(short, long)]
+    upper: bool,
+
+    /// Répeter un nombre de fois 
+    #[arg(short, long, default_value_t = 1)]
+    repeat: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    // Gestion de la mise en majuscules (Ownership) 
+    let mut display_name = args.name; // c'est une string pour déplacer la donnée vers la variable display_name. On la rend mut pour possible modification
+    
+    if args.upper {
+        display_name = display_name.to_uppercase();
+    }
+
+    // Boucle pour la répétition 
+    for _ in 0..args.repeat {
+        println!("Hello, {}!", display_name);
+    }
 }
